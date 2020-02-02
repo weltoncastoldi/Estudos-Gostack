d0404e3c5679e6d006670cb6baf05be090d0146e
Welton Castoldi <weltoncastoldi@hotmail.com>
2020-02-02 14:52:04 -0300

    Finalizada Aula de Cadastro  de usuarios

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({message: 'opa'})
-})
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'welton castoldi',
+    email: 'weltoncastoldi@hotmail.com',
+    password_hash: '123',
+  });
+  return res.json(user);
+});
 
 export default routes;
